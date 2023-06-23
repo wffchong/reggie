cd83b9146c793c98bed8d18ae9f06b175c619683
wff <283953455@qq.com>
2023-06-23 13:13:48 +0800

    feat: 分页查询菜品信息

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,14 +1,20 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -18,10 +24,60 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto) {
         log.info(dishDto.toString());
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功");
+    }
+
+    /**
+     * 菜品信息分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // dish里面没有categoryName，所以需要自己写一个dto
+        // DishDto里面有 categoryName
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.eq(name != null, Dish::getName, name);
+
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        dishService.page(pageInfo, queryWrapper);
+
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+
+        List<Dish> records = pageInfo.getRecords();
+
+        // 通过stream流，将records转换成dishDto
+        List<DishDto> list = records.stream().map(item -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
     }
 }
