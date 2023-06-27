c42a7c7923ef9a8edbd3f07df3265346f34052a5
wff <283953455@qq.com>
2023-06-27 16:22:21 +0800

    feat: 减少菜品

--- src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -95,4 +95,34 @@ public class ShoppingCartController {
 
         return R.success("清空购物车成功");
     }
+
+    /**
+     * 减少购物车中的菜品或者套餐
+     */
+
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        Long setmealId = shoppingCart.getSetmealId();
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+
+        if (setmealId != null) {
+            queryWrapper.eq(ShoppingCart::getSetmealId, setmealId);
+        } else {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        }
+        ShoppingCart one = shoppingCartService.getOne(queryWrapper);
+        Integer number = one.getNumber();
+        if (number == 1) {
+            one.setNumber(0);
+            shoppingCartService.remove(queryWrapper);
+        } else {
+            one.setNumber(number - 1);
+            shoppingCartService.updateById(one);
+        }
+
+        return R.success(one);
+    }
+
 }
